2743af078a16c323304166762d5716e0ecbbc0ca
golee922 <golee922@gmail.com>
2021-05-24 19:36:11 +0900

    Salting and Hashing Passwords with bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -38,38 +39,41 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    const username = req.body.username;
-    const password = md5(req.body.password);
 
-    const newUser = new User({
-        email : username,
-        password : password,
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        const username = req.body.username;
+        const password = hash;
+    
+        const newUser = new User({
+            email : username,
+            password : password,
+        });
+    
+        newUser.save(err => {
+            if(!err) {
+                res.render("secrets");
+            }
+            else {
+                console.log(err);
+            }
+        });
     });
 
-    newUser.save(err => {
-        if(!err) {
-            res.render("secrets");
-        }
-        else {
-            console.log(err);
-        }
-    });
 });
 
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email:username}, (err, foundUser) => {
         if(!err) {
             if(foundUser) {
-                if(foundUser.password === password) {
-                    res.render("secrets");   
-                }
-                else {
-
-                }
+                bcrypt.compare(password, foundUser.password, (err, result) => {
+                    if(result === true) {
+                        res.render("secrets");
+                    }
+                });
             }
         }
         else {
